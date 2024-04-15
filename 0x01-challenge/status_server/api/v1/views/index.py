7add5908ff7ad91ba77c3d7956592b0a8802f55a
LINUSOBURA <linusobura@gmail.com>
2024-04-15 16:33:41 +0300

    modified import path on index.py

--- 0x01-challenge/status_server/api/v1/views/index.py
+++ 0x01-challenge/status_server/api/v1/views/index.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """ Index view
 """
+from api.v1.app import app_views
 from flask import jsonify
-
-from api.v1.views import app_views
 
 
 @app_views.route('/api/v1/status', methods=['GET'], strict_slashes=False)
